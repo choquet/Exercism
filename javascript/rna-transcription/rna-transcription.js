aba8a3dc6861d8cd44e0cf8122435a29e9b10402
Antoine Choquet <choquet.antoine@gmail.com>
2019-11-28 19:28:39 +0100

    JS - rna-transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,1 +1,34 @@
-export const toRna = dna => dna.replace(/A/g, "U").replace(/T/g, "A").replace(/G/g, "_").replace(/C/g, "G").replace(/_/g, "C")
+const TRANSCRIPTION = {
+  A: "U",
+  T: "A",
+  G: "C",
+  C: "G"
+}
+
+/**
+ * Array.reduce solution
+ * 
+ * Better performance than Regexp and Array.map solutions
+ *
+ * @param {String} dna
+ * @return {String} rna
+ */
+export const toRna = dna => dna.split('').reduce((acc, val) => acc + TRANSCRIPTION[val], "")
+
+/**
+ * Array.map solution
+ * 
+ * Better perf than Regexp solution
+ * 
+ * @param {String} dna
+ * @return {String} rna
+ */
+export const toRna1 = dna => dna.split('').map((val) => TRANSCRIPTION[val]).join("")
+
+/**
+ * Regexp solution
+ * 
+ * @param {String} dna
+ * @return {String} rna
+ */
+export const toRna2 = dna => dna.replace(/[ATCG]/g, x => TRANSCRIPTION[x])
